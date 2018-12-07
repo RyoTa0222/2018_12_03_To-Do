25180b36a663a152d57f60d7492bad2febb92002
matsumotoryouta <r.matsumoto.creative.innovation@gmail.com>
2018-12-08 07:53:33 +0900

    # App.js  - 取り消し機能の実装（list.jsの更新ボタンで第二引数にtitleを持ってくることで可能に）  - dialogを取り入れて更新機能の実装
    # list.js
     -

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@
 import React, { Component } from 'react';
 import {List} from './list.js'
 import {Input} from './input.js'
+//import {Dialog} from './dialog.js'
 import './App.css'
+import Loader from 'react-loader-spinner'
 
 
 //親コンポーネント(app)
@@ -30,21 +32,19 @@ class App extends Component {
         this.state.todo.push({
             title: value
         });
-        //保存
+        //更新
         this.setState({
             todo : this.state.todo
         });
     }
     //取り消し機能
-    deleteTodo(i){
+    deleteTodo(todo, i){
         //コピー
         const todo_delete = this.state.todo.slice();
-        //todo_delete[i] = (({title}) =>
-        //    ({title: <del>{title}</del>}));
 
         //取り消し線
-        todo_delete[i] = {title: <del>delete</del>};
-        //保存
+        todo_delete[i] = {title: <del>{todo.title}</del>};
+        //更新
         this.setState({
             todo : todo_delete
         });
@@ -54,9 +54,11 @@ class App extends Component {
     changeTodo(i){
         //コピー
         const todo_update = this.state.todo.slice();
-        //todo_update[i] ={title: ''};
-        todo_update[i] = {title: 'Update'};
-        //保存
+        //dialog表示
+        const dialog = window.prompt("メモを記入してください", "");
+        //変更
+        todo_update[i] = {title: dialog};
+        //更新
         this.setState({
             todo : todo_update
         });
@@ -74,6 +76,12 @@ class App extends Component {
                 deleteTodo={this.deleteTodo}
                 changeTodo={this.changeTodo}/>
           <Input addTodo={this.addTodo} />
+          <Loader
+              type="CradleLoader"
+              color="#00BFFF"
+              height="100"
+              width="100"
+          />
       </div>
     );
   }
--- src/list.js
+++ src/list.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 export function List(props){
     return(
         <ul className="ul_todo">
@@ -8,7 +7,7 @@ export function List(props){
                 return <li key={i}>
                     {todo.title}
                     <div className="btn_list_position">
-                        <button onClick={() => props.deleteTodo(i)} className="button button_delete">完了</button>
+                        <button onClick={() => props.deleteTodo(todo, i)} className="button button_delete">完了</button>
                         <button onClick={() => props.changeTodo(i)}  className="button button_change">変更</button>
                     </div>
                     {/*<button onClick={() => props.storageTodo(i)} className="button_storage">保存</button>*/}
